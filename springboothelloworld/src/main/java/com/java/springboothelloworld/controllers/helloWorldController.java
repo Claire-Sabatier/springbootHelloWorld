2d18ed712e31563b328dd3ede70e456329980cf6
Claire Sabatier <claire08.sabatier@gmail.com>
2022-02-21 17:20:51 +0100

    erreur2

--- springboothelloworld/src/main/java/com/java/springboothelloworld/controllers/helloWorldController.java
+++ springboothelloworld/src/main/java/com/java/springboothelloworld/controllers/helloWorldController.java
@@ -11,17 +11,7 @@ public class helloWorldController {
         produces = "text/plain"
     )
     public String getHelloWorld(){
-        return "Hello World !";
-    }
-
-    @GetMapping(
-        path = "/erreur",
-        produces = "text/plain"
-    )
-    public String getCasserCode(){
-        if(2+2==5){
-            return "oui";
-        }
+        return Hello World !;
     }
 
 
