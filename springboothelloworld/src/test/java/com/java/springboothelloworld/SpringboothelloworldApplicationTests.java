5e938511ef0a1c3d17a0fae63e6f4902a07cea95
Claire Sabatier <claire08.sabatier@gmail.com>
2022-02-22 15:21:37 +0100

    test juste

--- springboothelloworld/src/test/java/com/java/springboothelloworld/SpringboothelloworldApplicationTests.java
+++ springboothelloworld/src/test/java/com/java/springboothelloworld/SpringboothelloworldApplicationTests.java
@@ -1,5 +1,7 @@
 package com.java.springboothelloworld;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class SpringboothelloworldApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals("true", "true");
 	}
 
 }
